a4b9f54b758194c0766fe5caa4fb0bb19f1e57b9
Chris Forbes <chrisf@ijw.co.nz>
2011-04-18 14:54:46 +1200

    add some nice histogram passes for ralint

--- OpenRA.Mods.RA/Lint/LintBuildablePrerequisites.cs
+++ OpenRA.Mods.RA/Lint/LintBuildablePrerequisites.cs
@@ -39,4 +39,58 @@ namespace OpenRA.Mods.RA
 			}
         }
 	}
+	
+	class LintBuildActorSizeHistogram : ILintPass
+	{
+		public void Run(Action<string> emitError)
+		{
+			var sizes = new Dictionary<int,int>();
+			
+			foreach( var i in Rules.Info )
+			{
+				if (i.Key.StartsWith("^"))
+					continue;
+			
+				var size = i.Value.Traits.WithInterface<object>().Count();
+				if (!sizes.ContainsKey(size))
+					sizes[size] = 1;
+				else
+					sizes[size]++;
+			}
+			
+			foreach( var s in sizes.OrderByDescending( kv => kv.Key ) )
+			{
+				Console.WriteLine ("{0} {1}", s.Key.ToString().PadLeft(4), new string('+',s.Value) );
+			}
+		}
+	}
+	
+	class LintPopularTraitsHistogram : ILintPass
+	{
+		public void Run(Action<string> emitError)
+		{
+			var sizes = new Dictionary<string,int>();
+			
+			foreach( var i in Rules.Info )
+			{
+				if (i.Key.StartsWith("^"))
+					continue;
+			
+				foreach( var t in i.Value.Traits.WithInterface<object>().Select( a => a.GetType().Name.Replace("Info","") ) )
+					if (!sizes.ContainsKey(t))
+						sizes[t] = 1;
+					else
+						sizes[t]++;
+			}
+			
+			var w = sizes.Keys.Max(k => k.Length);
+			var q = sizes.Values.Max();
+			var r = sizes.Values.Max(k => k.ToString().Length);
+			
+			var z = 80 - 4 - w - r;	/* roughly: space left in term */
+			
+			foreach( var s in sizes.OrderByDescending( kv => kv.Value ) )
+				Console.WriteLine ("{0}:{2} {1}", s.Key.ToString().PadLeft(w), new string('+', (s.Value * z + q - 1) / q), s.Value.ToString().PadLeft(r) );
+		}
+	}
 }
