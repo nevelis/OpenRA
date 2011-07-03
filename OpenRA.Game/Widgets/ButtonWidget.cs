6656848efe548030e575593f5c1a90d51e556359
Paul Chote <paul@chote.net>
2011-07-04 01:51:38 +1200

    Remove Widget.OnMouseMove.

--- OpenRA.Game/Widgets/ButtonWidget.cs
+++ OpenRA.Game/Widgets/ButtonWidget.cs
@@ -90,15 +90,9 @@ namespace OpenRA.Widgets
 				else
 					LoseFocus(mi);
 			}
-			
 			else if (mi.Event == MouseInputEvent.Move && Focused)
-			{
 				Depressed = RenderBounds.Contains(mi.Location.X, mi.Location.Y);
-				
-				// All widgets should receive MouseMove events
-				OnMouseMove(mi);
-			}
-			
+
 			return Depressed;
 		}
 
